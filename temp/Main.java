4f4efa1fb03821fd68d2e0b033bc079576d1d08a
Dushyant Baisla <dushyant.baisla@gmail.com>
2021-08-16 00:34:49 +0530

    ....

---
+++ temp/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        for(int b =2; b<3 ; b+=2){
+            System.out.print(b+ ++b);
+        }
+    }
+
+}
--- temp/temp.js
+++ temp/temp.js
@@ -1,8 +1,22 @@
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+// arguments
+// function f(){
+//     let sum = 1;
 
-  let x = getRandomInt(500, 1000);
-  let y = getRandomInt(500, 1000);
-  let ran = getRandomInt(x, y)
-  console.log(ran);
+//     for(let x in arguments){        
+//         x != 0 ? sum*= arguments[x] : sum *= 1;
+//     }
+//     return sum;
+// }
+// console.log(f(1,2,3));
+
+//----------- Strings
+/* 
+    Strings are immutable and all string functions will return a new string without changing the origional string
+
+    #1 str.replace(replace what, replace with)
+    #2 str.slice(idx) || str.slice(start, end)
+    #3 str.toUpperCase() => changes to upper case letters
+    #4 str.trim() => removes unwanted spaces in string
+    #5 str.split(splitting parameter) => splits array according to parameter and returns it in an array
+*/
+
